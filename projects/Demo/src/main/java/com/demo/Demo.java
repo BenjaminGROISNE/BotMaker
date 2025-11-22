bcac8cbe71be650ac5a44eb8a6ab815b8be64baa
groisnebenjamin <bgroisne@orange.fr>
2025-11-22 15:49:18 +0100

    added literal character format

--- projects/Demo/src/main/java/com/demo/Demo.java
+++ projects/Demo/src/main/java/com/demo/Demo.java
@@ -14,11 +14,10 @@ public class Demo {
 		boolean flag875 = false;
 		double decimal = 0;
 		text += flag875;
-		ArrayList<ArrayList<Integer>> myList5=new ArrayList<ArrayList<Integer>>(Arrays.asList(new ArrayList<Integer>(Arrays.asList(0,0))));
+		float myList5=5;
 		if (flag2) {
 			System.out.println("878");
 		}
-		ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>> myList=new ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>>(Arrays.asList(new ArrayList<ArrayList<ArrayList<Integer>>>(Arrays.asList(new ArrayList<ArrayList<Integer>>(Arrays.asList(new ArrayList<Integer>(Arrays.asList(null))))))));
 		while (flag) {
 			System.out.println(decimal);
 		}
